ecda8e5253100a1a7799b48ac2c4c8f1835841c5
Facundodandrea <facundodandrea01@gmail.com>
2024-01-09 13:56:19 -0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../src/components/navbar/Navbar'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <BrowserRouter>
+    <div className="App bg-grey">
+      <Navbar />
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+// Navbar.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+import brand from '../../assets/img/brand.svg';
+import { IoClose } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className={`nav-container ${isMenuOpen ? 'menu-open' : ''}`}>
+      <div className="nav-content">
+        <div className="brandContainer flex">
+          <img src={brand} alt="brand-logo" />
+          <p>PSIKO</p>
+        </div>
+        <button className="menu-button" onClick={toggleMenu}>
+          <IoMenu className='icon'/>
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div className="menu">
+            <div className="head-menu">
+                <h3>Menú</h3>
+                <button className="close-button" onClick={closeMenu}>
+                    <IoClose className='icon'/>
+                    <p>Cerrar</p>
+                </button>
+            </div>
+            <ul>
+              <li><Link to="/   ">Artículos</Link></li>
+            </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
